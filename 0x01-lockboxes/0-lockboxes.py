662038704335184fa5629997c1e3296f76b067f6
jamal <jamalimmiss97@gmail.com>
2024-10-10 22:15:15 +0100

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a module that provides a function for determining if all
+boxes in a given list can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    This function takes a list of lists and returns a boolean
+
+    Parameters:
+    boxes (List[List[int]]): The list of lists representing the boxes
+        and their keys.
+
+    Returns:
+    bool: True if all boxes can be opened, else False.
+    """
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
